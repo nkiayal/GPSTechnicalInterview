ef53ab83ae63ca7c684a94438a525f2f1a042b08
ColeHend <darkgale93@gmail.com>
2022-11-19 13:57:34 -0700

    backend close to done

--- GPS.TechnicalInterview.Web/Controllers/ApplicationManagerController.cs
+++ GPS.TechnicalInterview.Web/Controllers/ApplicationManagerController.cs
@@ -2,20 +2,48 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using GPS.ApplicationManager.Web;
 namespace GPS.ApplicationManager.Web.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  public class ApplicationManagerController : ControllerBase
-  {
-    private readonly ILogger<ApplicationManagerController> _logger;
-
-    public ApplicationManagerController(ILogger<ApplicationManagerController> logger)
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationManagerController : ControllerBase
     {
-      _logger = logger;
-    }
+        private readonly ILogger<ApplicationManagerController> _logger;
 
-    // TODO: Add your CRUD (Create, Read, Update, Delete) methods here:
-  }
+        public ApplicationManagerController(ILogger<ApplicationManagerController> logger)
+        {
+            _logger = logger;
+        }
+
+        // TODO: Add your CRUD (Create, Read, Update, Delete) methods here:
+
+        JsonDatabase database = new JsonDatabase();
+        [HttpGet]
+        public async Task<ActionResult<List<LoanApplication>>> Get()
+        {
+            var theData = await database.GetApp();
+            return Ok(theData);
+        }
+        [HttpGet("{AppNumber}")]
+        public async Task<ActionResult<LoanApplication>> GetOneApp(string AppNumber)
+        {
+            var theData = await database.GetOneApp(AppNumber);
+            return Ok(theData);
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> SaveApp(LoanApplication loanApplication)
+        {
+            await database.SaveApplication(loanApplication);
+            return Ok();
+        }
+        [HttpDelete("{AppNumber}")]
+        public async Task<ActionResult> DeleteApp(string AppNumber)
+        {
+            await database.deleteApplication(AppNumber);
+            return Ok();
+        }
+    }
 }
---
+++ GPS.TechnicalInterview.Web/LoanAppModal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GPS.ApplicationManager.Web
+{
+    public class LoanTerms
+    {
+        [Key]
+        public double Amount { get; set; }
+        public double MonthlyPaymentAmount { get; set; }
+        public uint Term { get; set; }
+
+    }
+    public class Name
+    {
+        [Key]
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+    public class PersonalInfo
+    {
+        [Key]
+        public Name Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+    public class LoanApplication
+    {
+        [Key]
+        public string ApplicationNumber { get; set; }    
+        public LoanTerms loanTerms { get; set; }
+        public PersonalInfo PersonalInformation { get; set; }
+        public DateOnly DateApplied { get; set; }
+        public Enum Status { get; set; }
+
+    }
+}
