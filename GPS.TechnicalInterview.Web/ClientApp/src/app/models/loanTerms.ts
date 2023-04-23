dc534574c8e381736fb7cf2e3c56fb45375adb81
Daniel Bigelow <danielbigelowse@gmail.com>
2023-04-22 20:24:43 -0600

    Added models to client app. Added endpoint for getting all loan applications in API

---
+++ GPS.TechnicalInterview.Web/ClientApp/src/app/models/loanTerms.ts
@@ -0,0 +1,7 @@
+export interface LoanTerms {
+    amountDollars: number;
+    amountCents: number;
+    monthlyPaymentAmountDollars: number;
+    monthlyPaymentAmountCents: number;
+    term: number;
+}
