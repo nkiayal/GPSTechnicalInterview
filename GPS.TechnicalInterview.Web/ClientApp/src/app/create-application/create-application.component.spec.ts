c9580c1d11f22791641046832c11c7ecc53729b3
jpe0824 <jason.edman@microfocus.com>
2023-05-16 14:24:18 -0600

    Updates to routing

--- GPS.TechnicalInterview.Web/ClientApp/src/app/create-application/create-application.component.spec.ts
+++ GPS.TechnicalInterview.Web/ClientApp/src/app/create-application/create-application.component.spec.ts
@@ -1,16 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CreateApplicationComponent } from './create-application.component';
+import { CreateApplicationComponent } from "./create-application.component";
 
-describe('CreateApplicationComponent', () => {
+describe("CreateApplicationComponent", () => {
   let component: CreateApplicationComponent;
   let fixture: ComponentFixture<CreateApplicationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateApplicationComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateApplicationComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('CreateApplicationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
