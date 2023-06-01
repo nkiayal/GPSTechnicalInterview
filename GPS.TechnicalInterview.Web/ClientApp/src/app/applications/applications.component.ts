875fff6f8506127277c933854737bfb122c2963b
Talon Durrant <talonmdurrant@gmail.com>
2023-05-31 21:31:28 -0600

    pull in application data and display on table show 3-dot menu icon on each row with a mat-menu

--- GPS.TechnicalInterview.Web/ClientApp/src/app/applications/applications.component.ts
+++ GPS.TechnicalInterview.Web/ClientApp/src/app/applications/applications.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Application } from '../interfaces/Application';
 
 @Component({
   selector: 'app-applications',
   templateUrl: './applications.component.html',
-  styleUrls: ['./applications.component.scss']
+  styleUrls: ['./applications.component.scss'],
 })
 export class ApplicationsComponent {
+  public displayedColumns: Array<string> = ['applicationNumber', 'amount', 'dateApplied', 'status', 'actions'];
 
-  public displayedColumns: Array<string> = ['applicationNumber', 'amount', 'dateApplied', 'status']; 
+  public applications: Application[] = [];
 
-  constructor() { }
+  constructor(api: ApiService) {
+    api.getAllApplications().subscribe(applications => {
+      console.log(applications);
+      this.applications = applications;
+    });
+  }
 }
--- GPS.TechnicalInterview.Web/ClientApp/src/app/shared/material.module.ts
+++ GPS.TechnicalInterview.Web/ClientApp/src/app/shared/material.module.ts
@@ -5,12 +5,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [
-      MatCardModule, MatFormFieldModule, MatTableModule, 
-      MatSelectModule, MatInputModule, MatButtonModule,
-    ],
+  exports: [MatCardModule, MatFormFieldModule, MatTableModule, MatSelectModule, MatInputModule, MatButtonModule, MatMenuModule, MatIconModule],
 })
 export class MaterialModule {}
