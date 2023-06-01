699797d4fc1d7eaf4680ef3d9a96b5fbc58a6dc3
Talon Durrant <talonmdurrant@gmail.com>
2023-05-31 21:29:37 -0600

    wrote methods in service for working with api

--- GPS.TechnicalInterview.Web/ClientApp/src/app/api.service.ts
+++ GPS.TechnicalInterview.Web/ClientApp/src/app/api.service.ts
@@ -1,7 +1,44 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Application } from './interfaces/Application';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ApiService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
+
+  getAllApplications(): Observable<Application[]> {
+    return this.http.get<Application[]>(this.baseUrl + 'api/applicationmanager').pipe(catchError(this.handleError));
+  }
+
+  getApplication(applicationNumber: string): Observable<Application[]> {
+    return this.http.get<Application[]>(this.baseUrl + `api/applicationmanager/${applicationNumber}`).pipe(catchError(this.handleError));
+  }
+
+  createApplication(application: Application): Observable<boolean> {
+    return this.http.post<null>(this.baseUrl + 'api/applicationmanager', application).pipe(
+      map(() => true),
+      catchError(this.handleError)
+    );
+  }
+
+  updateApplication(applicationNumber: string, application: Application): Observable<boolean> {
+    return this.http.put<null>(this.baseUrl + `api/applicationmanager/${applicationNumber}`, application).pipe(
+      map(() => true),
+      catchError(this.handleError)
+    );
+  }
+
+  deleteApplication(applicationNumber: string): Observable<boolean> {
+    return this.http.delete<null>(this.baseUrl + `api/applicationmanager/${applicationNumber}`).pipe(
+      map(() => true),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('An error occurred: ', error);
+    return throwError(() => new Error('An error occurred. Please try again later'));
+  }
 }
---
+++ GPS.TechnicalInterview.Web/ClientApp/src/app/interfaces/Application.ts
@@ -0,0 +1,24 @@
+export interface Application {
+  applicationNumber: string;
+  dateApplied: string;
+  status: number;
+  loanTerms: LoanTerms;
+  personalInformation: PersonalInformation;
+}
+
+type LoanTerms = {
+  amount: number;
+  monthlyPaymentAmount: number;
+  term: number;
+};
+
+type Name = {
+  first: string;
+  last: string;
+};
+
+type PersonalInformation = {
+  name: Name;
+  phoneNumber: string;
+  email: string;
+};
