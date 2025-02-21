3f5548d7acd72873c5d394d9f6f28d1fc402103e
Kiaya Neibaur <kneibaur@dhicorp.com>
2025-02-21 13:38:52 -0700

    Add the create controller and the LoanApplication object.

--- GPS.TechnicalInterview.Web/Controllers/ApplicationManagerController.cs
+++ GPS.TechnicalInterview.Web/Controllers/ApplicationManagerController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GPS.ApplicationManager.Web.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace GPS.ApplicationManager.Web.Controllers
 {
@@ -10,12 +13,45 @@ namespace GPS.ApplicationManager.Web.Controllers
   public class ApplicationManagerController : ControllerBase
   {
     private readonly ILogger<ApplicationManagerController> _logger;
+    private static readonly string _filePath = "loanApplication.json";
 
     public ApplicationManagerController(ILogger<ApplicationManagerController> logger)
     {
       _logger = logger;
     }
 
-    // TODO: Add your CRUD (Create, Read, Update, Delete) methods here:
+    private async static Task<List<LoanApplication>> GetApplicationsFromFileAsync()
+    {
+      if (System.IO.File.Exists(_filePath))
+      {
+        var existingJson = await System.IO.File.ReadAllTextAsync(_filePath);
+        return JsonSerializer.Deserialize<List<LoanApplication>>(existingJson) ?? new List<LoanApplication>();
+      }
+      return new List<LoanApplication>();
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> CreateApplication([FromBody] LoanApplication loanApplication)
+    {
+      if (loanApplication == null ||
+          string.IsNullOrEmpty(loanApplication.PersonalInformation.Name.First) ||
+          string.IsNullOrEmpty(loanApplication.PersonalInformation.Name.Last) ||
+          string.IsNullOrEmpty(loanApplication.PersonalInformation.PhoneNumber) ||
+          string.IsNullOrEmpty(loanApplication.PersonalInformation.Email) ||
+          string.IsNullOrEmpty(loanApplication.ApplicationNumber) ||
+          loanApplication.LoanTerms.Amount <= 0)
+      {
+        return BadRequest("Invalid application data. All fields are required and must be valid.");
+      }
+
+      loanApplication.DateApplied = DateTime.UtcNow;
+      var applications = await GetApplicationsFromFileAsync();
+      applications.Add(loanApplication);
+      var json = JsonSerializer.Serialize(applications);
+      await System.IO.File.WriteAllTextAsync(_filePath, json);
+      return Ok(new { message = "Created Successfully." });
+    }
+
+    // TODO: Add your CRUD (Read, Update, Delete) methods here:
   }
 }
---
+++ GPS.TechnicalInterview.Web/Controllers/Models/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace GPS.ApplicationManager.Web.Controllers.Models
+{
+  public enum ApplicationStatus
+  {
+    New = 0,
+    Approved = 1,
+    Funded = 2
+  }
+}
